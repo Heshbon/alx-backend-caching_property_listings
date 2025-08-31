9c32b018b2863d2fc775aac22cfd5bb755117ec7
Hesbon <cityalight.hesbon@gmail.com>
2025-08-31 17:55:24 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,19 +1,19 @@
 from django.views.decorators.cache import cache_page
-from django.utils.decorators import method_decorator
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import Property
+from rest_framework.decorators import api_view
 
 
-class PropertyListView(APIView):
-    @method_decorator(cache_page(60 * 15))  # 15 minutes
-    def get(self, request):
-        properties = Property.objects.all()
-        return Response([{
-            'id': prop.id,
-            'title': prop.title,
-            'description': prop.description,
-            'price': float(prop.price),
-            'location': prop.location,
-            'created_at': prop.created_at
-        } for prop in properties])
+@api_view(['GET'])
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.objects.all()
+    data = [{
+        'id': prop.id,
+        'title': prop.title,
+        'description': prop.description,
+        'price': float(prop.price),
+        'location': prop.location,
+        'created_at': prop.created_at
+    } for prop in properties]
+    return Response(data)
